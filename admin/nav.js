f797f88ab3dd3400616e4dbd82322f09d87ad35a
christopherstrasser-sudo <christopher.strasser@gmx.de>
2026-09-09 08:32:18 +0200

    Bump admin control center to v0.9.4

--- admin/nav.js
+++ admin/nav.js
@@ -40,7 +40,7 @@
   if (logout && !document.querySelector('.admin-side-meta')) {
     const meta = document.createElement('div');
     meta.className = 'admin-side-meta';
-    meta.innerHTML = '<span>PALPANEL</span><b>v0.9.3</b>';
+    meta.innerHTML = '<span>PALPANEL</span><b>v0.9.4</b>';
     logout.insertAdjacentElement('beforebegin', meta);
   }
 
